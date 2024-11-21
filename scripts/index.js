54195f0e70d6f8b0fe13fbe44416a45774f6e82b
Kyle Welchner <kaithaire@gmail.com>
2024-11-21 15:00:47 -0500

    Update Modal open and close

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,19 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector(".profile__edit-button");
+
+const editModal = document.querySelector("#edit-modal");
+const editModalClose = editModal.querySelector(".modal__close-button");
+
+function openModal() {
+  editModal.classList.add("modal_opened");
+}
+
+function closeModal() {
+  editModal.classList.remove("modal_opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+
+editModalClose.addEventListener("click", closeModal);
